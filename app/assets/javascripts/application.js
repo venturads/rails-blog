79edf21d2c2fc25b33c2dbe2d7f2fd45b19cf554
Albert Ventura <albert@venturads.com>
2017-06-14 04:32:40 +0000

    added comments

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require turbolinks
+//= require_tree .
+
+
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 5) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+    before_action :find_post
+    
+    
+    def create
+        @comment = @post.comments.create(params[:comment].permit(:content))
+        @comment.user_id = current_user.id
+        @comment.save
+        redirect_to post_path(@post)
+    end
+    def destroy
+        @comment = @post.comment.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
+
+    private
+    
+    def find_post
+        @post = Post.find(params[:post_id])
+    end
+   
+end
