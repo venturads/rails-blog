f7041044293d1c6c16f8ef0e03a90d93ca57048e
Albert Ventura <albert@venturads.com>
2017-06-16 21:06:31 +0000

    fix routes

--- db/migrate/20170615055328_rename_user_id_id_to_user_id.rb
+++ db/migrate/20170615055328_rename_user_id_id_to_user_id.rb
@@ -1,5 +1,5 @@
 class RenameUserIdIdToUserId < ActiveRecord::Migration
   def change
-    rename_column :profiles, :user_id_id, :user_id
+    #rename_column :profiles, :user_id_id, :user_id
   end
 end
