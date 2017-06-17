c92d0664cfe02238c7bae87d0892093f41302c75
Albert Ventura <albert@venturads.com>
2017-06-17 00:30:22 +0000

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-40.times do
+4.times do
     Post.create(title:"My Title",body:"my body text")
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,14 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  get "/posts/new"
+  assert_response :success
+ 
+  post "/posts",
+    params: { post: { title: "can create", body: "post successfully." } }
+  assert_response :redirect
+  follow_redirect!
+  assert_response :success
+  assert_select "p", "Title:\n  can create"
 end
